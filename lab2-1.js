a0f8c9f239b8443f3479f29890e47908d985b4fe
Teashechka <myshkoknysh@gmail.com>
2026-06-12 18:25:34 +0300

    Add functions to check integers and find primes

---
+++ lab2-1.js
@@ -0,0 +1,17 @@
+function isInteger(num) {
+    return Number.isInteger(num);
+}
+function findPrimes(a, b) {
+    console.log(`Прості числа від ${a} до ${b}:`);
+    for (let i = a; i <= b; i++) {
+        if (i < 2) continue;
+        let isPrime = true;
+        for (let j = 2; j <= Math.sqrt(i); j++) {
+            if (i % j === 0) {
+                isPrime = false;
+                break;
+            }
+        }
+        if (isPrime) console.log(i);
+    }
+}
